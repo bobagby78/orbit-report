86aa9ef8d241e4123ead76b3da81b066c9449cec
bobagby78 <bobagby78@gmail.com>
2021-04-04 16:19:57 -0500

    finished step 5

--- orbit-report/src/app/app.component.ts
+++ orbit-report/src/app/app.component.ts
@@ -12,14 +12,32 @@ export class AppComponent {
   sourceList: Satellite[];
 
   constructor() {
-    this.sourceList = [
-       new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-       new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-       new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-       new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-       new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
-    ];
- }
+    this.sourceList = [];
+   let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+
+   window.fetch(satellitesUrl).then(function(response) {
+      response.json().then(function(data) {
+
+         let fetchedSatellites = data.satellites; //variable for the fetched Satellite data
+         console.log (data.satellites)
+          for (let i = 0; i < fetchedSatellites.length; i++){ //loop through satellite fetch and ...
+            let newSatellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational); //create a satellite object and...
+            this.sourceList.push(newSatellite); //add the satellite to the sourceList to be displayed on the webpage
+         }
+ 
+      }.bind(this));
+   }.bind(this));
+
+  }
+//     this.sourceList = [
+//        new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
+//        new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
+//        new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
+//        new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
+//        new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
+//     ];
+//  }
+
 
 
 }
